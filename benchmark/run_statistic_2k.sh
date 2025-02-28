14f18a39f967fd26bebe887177356382468d9789
Shu-Wei Huang <N96114213@gs.ncku.edu.tw>
2025-02-28 22:10:35 +0800

    bash script updates

--- benchmark/run_statistic_2k.sh
+++ benchmark/run_statistic_2k.sh
@@ -2,7 +2,7 @@
 # conda activate logevaluate
 
 cd evaluation
-for technique in AEL Drain IPLoM LenMa LFA LogCluster LogMine Logram LogSig MoLFI SHISO SLCT Spell
+for technique in AEL Drain IPLoM LenMa LFA LogCluster LogLSHD LogMine Logram LogSig MoLFI SHISO SLCT Spell
 do
     echo ${technique}
     python ${technique}_eval.py -otc
